0ddb78af486014f96a9e1d2731d7b46033bf67f0
Nnurbek <82235115+Nnurbek@users.noreply.github.com>
2024-07-03 13:48:12 +0500

    Update views.py

--- file_upload/views.py
+++ file_upload/views.py
@@ -3,12 +3,6 @@ from .models import Assignment
 from .open_edx_api import get_courses
 from .forms import AssignmentForm
 from django.http import JsonResponse
-
- 
-def api_get_files(request):
-    files = Assignment.objects.values('id', 'course__course_id', 'student_id', 'file', 'uploaded_at')
-    return JsonResponse(list(files), safe=False)
-
  
 def list_courses(request):
     courses = get_courses()
